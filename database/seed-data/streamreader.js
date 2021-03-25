78a6bb3019e6e8c90f450572c787e5d1404cbdac
Benntobox <benjaminsboyle3@gmail.com>
2021-03-25 00:49:44 -0700

    readData now able to parse csv file into arrays and return data

--- database/seed-data/streamreader.js
+++ database/seed-data/streamreader.js
@@ -1,29 +1,32 @@
 const fs = require('fs');
+const util = require('util');
 
-let filepath = 'database/seed-data/csvData/data.csv';
+let datapath = 'database/seed-data/csvData/data.csv';
+
+let disassembler = files => {
+  let result = {};
+  let data = files.split('\n');
+  let headers = data.shift().split(',');
+  result.headers = headers;
+  for (let i = 0; i < data.length-1; i++) {
+    let record = data[i];
+    record = record.split(',');
+    record = record.slice(1, record.length-1);
+    result[i] = record;
+  }
+  return result;
+}
 
 let reader = async (file) => {
-  const stream = fs.createReadStream(file, {encoding: 'utf8'});
-  let result = {};
-
-  stream.on('data', file => {
-    let data = file.split('\n');
-    let headers = data.shift().split(',');
-    result.headers = headers;
-    for (let i = 0; i < data.length-1; i++) {
-      let record = data[i];
-      record = record.split(',');
-      record = record.slice(1, record.length-1);
-      result[i] = record;
-    }
-    return result;
-  })
-
-  stream.on('close', () => {
-    console.log('FINISHED!');
-    return result;
+  return new Promise((resolve, reject) => {
+    const stream = fs.createReadStream(file, {encoding: 'utf8'});
+    stream.on('error', reject);
+    stream.on('data', data => resolve(disassembler(data)));
   });
 }
 
+let readData = async (filepath) => await reader(filepath);
 
-(async () => console.log(await reader(filepath)))();
+modules.export.readData = readData;
+
+// (async () => console.log(await readData(datapath)))();
